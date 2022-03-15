25a808e301a0fb55a7946c72a9df74907b8c7ae8
Roodz Fernando Fleurant <fleurantrfernando@gmail.com>
2022-03-15 10:21:17 -0400

    Init Setup

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const port = process.env.PORT || 3000
+const print = require('../utils')
+
+const app = express()
+
+app.get('/', (req, res) =>{
+  res.send('Hello')
+})
+
+app.listen(port, ()=> {
+  print(`Server is running on port ${port}`)
+})
