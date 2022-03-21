efd71eb877e6c503a80a8f8001c4e3745bfb1562
Afam Ifeanyi(Chrys) M <afam_ifeanyi@live.com>
2022-03-21 16:59:55 +0200

    Add show all users to userRoutes

--- model/user.js
+++ model/user.js
@@ -1,38 +1,34 @@
 const mongoose = require('mongoose');
-const {deptSchema} = require('../model/department');
-
+const { deptSchema } = require('../model/department');
 
 const userSchema = new mongoose.Schema({
-    firstName: {
-      type: String,
-      minlength: 1,
-      maxlength: 50,
-      required: true
-    },
-    lastName: {
-      type: String,
-      minlength: 1,
-      maxlength: 50,
-      required: true
-    },
-    date_started: {
-      type: Date,
-      default: Date.now()
-    },
-    dept: {
-      type: String,
-      required: true
-    },
-    role: {
-      type: String,
-      enum: ['Administrator', 'Student', 'Lecturer'],
-      required: true
-    }
-  });
+  firstName: {
+    type: String,
+    minlength: 1,
+    maxlength: 50,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    minlength: 1,
+    maxlength: 50,
+    required: true,
+  },
+  date_started: {
+    type: Date,
+    default: Date.now(),
+  },
+  dept: {
+    type: deptSchema,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['Administrator', 'Student', 'Lecturer'],
+    required: true,
+  },
+});
 
-  const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
-  module.exports = User;
-
-
-
+module.exports = User;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,32 +1,34 @@
-const express = require('express')
-const User = require('../model/user')
-const {Department} = require('../model/department')
-const route = express.Router()
-const print = require('../utils')
+const express = require('express');
+const User = require('../model/user');
+const { Department } = require('../model/department');
+const route = express.Router();
+const print = require('../utils');
 
 // Create User
 route.post('/', async (req, res) => {
-  const {
-    firstName,
-    lastName,
-    date_started,
-    dept,
-    role
-  } = req.body
+  const { firstName, lastName, date_started, dept, role } = req.body;
 
   try {
-    const deptObj = await Department.findOne({name: dept})
+    const deptObj = await Department.findOne({ name: dept });
     const user = new User({
       ...req.body,
-      dept: deptObj
-    })
-    await user.save()
-    res.status(201).send(user)
+      dept: deptObj,
+    });
+    await user.save();
+    res.status(201).send(user);
   } catch (error) {
-    res.status(400).send(error)
+    res.status(400).send(error);
   }
-})
+});
 // Show Users
+route.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 // Show a User
 
@@ -34,4 +36,4 @@ route.post('/', async (req, res) => {
 
 // Delete a User
 
-module.exports = route
+module.exports = route;
