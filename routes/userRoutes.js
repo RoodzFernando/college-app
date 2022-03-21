22ae1e5057bf9a829c7b0728d9e5057e0ddef48a
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2022-03-21 11:19:56 -0400

    Finished the CRUD operations for Users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -32,8 +32,36 @@ route.get('/', async (req, res) => {
 
 // Show a User
 
+route.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    if (!user) return res.status(404).send({'message': 'User does not exist'})
+    res.send(user)
+  } catch (error) {
+    res.status(400).send(error)
+  }
+})
+
 // Update a User
 
+route.patch('/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    res.send(user)
+  } catch (error) {
+    res.status(400).send(error)
+  }
+})
+
 // Delete a User
+
+route.delete('/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id)
+    res.send(user)
+  } catch (error) {
+    res.status(400).send(error)
+  }
+})
 
 module.exports = route;
